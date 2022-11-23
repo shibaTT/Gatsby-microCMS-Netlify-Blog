29f32c1b6bca647bd4157c134bc5ff3a7084e46d
s1622074@s.do-johodai.ac.jp <s1622074@s.do-johodai.ac.jp>
2022-11-23 23:21:35 +0900

    update: スタイル微調整

--- src/pages/blog/{microcmsBlogs.blogsId}.js
+++ src/pages/blog/{microcmsBlogs.blogsId}.js
@@ -9,12 +9,13 @@ const BlogPage = ({ data }) => (
     <Seo title={data.microcmsBlogs.title} />
     <div className="c-post">
       <h1 className="c-post__title">{data.microcmsBlogs.title}</h1>
-      <Link to={`/category/${data.microcmsBlogs.category.id}`}>
-        <div className="c-post__category">
+      <div className="c-post__category">
+        <Link to={`/category/${data.microcmsBlogs.category.id}`}>
           {data.microcmsBlogs.category.name}
-        </div>
-      </Link>
+        </Link>
+      </div>
       <div
+        className="c-post__contents"
         dangerouslySetInnerHTML={{
           __html: `${data.microcmsBlogs.content}`,
         }}
