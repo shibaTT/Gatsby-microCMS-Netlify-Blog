86143b42ff00c0dbf521cae021bc0d8412dc0095
s1622074@s.do-johodai.ac.jp <s1622074@s.do-johodai.ac.jp>
2023-03-20 23:37:13 +0900

    update: シェアボタンの追加

--- src/pages/blog/{microcmsBlogs.blogsId}.js
+++ src/pages/blog/{microcmsBlogs.blogsId}.js
@@ -5,68 +5,92 @@ import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 
 const BlogPage = ({ data }) => (
-  <Layout currentPage="blog">
-    <Seo title={data.microcmsBlogs.title} />
-    <article className="c-post">
-      <div
-        className={`c-post__eyeCatch c-post__eyeCatch-${data.microcmsBlogs.category.id}`}
-      >
-        <p className="c-post__eyeCatchCategory">
-          {(() => {
-            if (data.microcmsBlogs.eyecatch === null) {
-              return data.microcmsBlogs.category.name
-            } else {
-              return (
-                <img
-                  srcSet={data.microcmsBlogs.eyecatch.url}
-                  alt={data.microcmsBlogs.title}
-                />
-              )
-            }
-          })()}
-        </p>
-      </div>
-      <div className="c-post__meta">
-        <h1 className="c-post__title">{data.microcmsBlogs.title}</h1>
-        {/* <div className="c-post__category">
-          <Link to={`/category/${data.microcmsBlogs.category.id}`}>
-            {data.microcmsBlogs.category.name}
-          </Link>
-        </div> */}
-        <div className="c-post__dates">
-          <p className="c-post__createdAt">{data.microcmsBlogs.createdAt}</p>
-          {data.microcmsBlogs.createdAt !== data.microcmsBlogs.updatedAt && (
-            <p className="c-post__updatedAt">{data.microcmsBlogs.updatedAt}</p>
-          )}
-        </div>
-      </div>
-      <div
-        className="c-post__contents"
-        dangerouslySetInnerHTML={{
-          __html: `${data.microcmsBlogs.content}`,
-        }}
-      />
-    </article>
-  </Layout>
+    <Layout currentPage="blog">
+        <Seo title={data.microcmsBlogs.title} />
+        <article className="c-post">
+            <div
+                className={`c-post__eyeCatch c-post__eyeCatch-${data.microcmsBlogs.category.id}`}
+            >
+                <p className="c-post__eyeCatchCategory">
+                    {(() => {
+                        if (data.microcmsBlogs.eyecatch === null) {
+                            return data.microcmsBlogs.category.name
+                        } else {
+                            return (
+                                <img
+                                    srcSet={data.microcmsBlogs.eyecatch.url}
+                                    alt={data.microcmsBlogs.title}
+                                />
+                            )
+                        }
+                    })()}
+                </p>
+            </div>
+            <div className="c-post__meta">
+                <h1 className="c-post__title">{data.microcmsBlogs.title}</h1>
+                {/* <div className="c-post__category">
+                <Link to={`/category/${data.microcmsBlogs.category.id}`}>
+                    {data.microcmsBlogs.category.name}
+                </Link>
+                </div> */}
+                <div className="c-post__dates">
+                    <p className="c-post__createdAt">
+                        {data.microcmsBlogs.createdAt}
+                    </p>
+                    {data.microcmsBlogs.createdAt !==
+                        data.microcmsBlogs.updatedAt && (
+                        <p className="c-post__updatedAt">
+                            {data.microcmsBlogs.updatedAt}
+                        </p>
+                    )}
+                </div>
+            </div>
+            <div
+                className="c-post__contents"
+                dangerouslySetInnerHTML={{
+                    __html: `${data.microcmsBlogs.content}`,
+                }}
+            />
+            <div className="c-post__share">
+                <p className="c-post__shareText">Share!</p>
+                <div className="c-post__sns">
+                    <a
+                        href={`https://twitter.com/intent/tweet?url=${data.site.siteMetadata.siteUrl}/blog/${data.microcmsBlogs.blogsId}&text=${data.microcmsBlogs.title}%20%7C%20To%20The%20First`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="c-post__snsItem is-twitter"
+                        aria-label="Twitterシェアボタン"
+                    >
+                        <span class="c-post__snsItemText">Twitterでシェア</span>
+                    </a>
+                </div>
+            </div>
+        </article>
+    </Layout>
 )
 
 export default BlogPage
 
 export const query = graphql`
-  query ($id: String!) {
-    microcmsBlogs(id: { eq: $id }) {
-      blogsId
-      title
-      content
-      updatedAt(formatString: "YYYY/MM/DD")
-      createdAt(formatString: "YYYY/MM/DD")
-      category {
-        name
-        id
-      }
-      eyecatch {
-        url
-      }
+    query ($id: String!) {
+        microcmsBlogs(id: { eq: $id }) {
+            blogsId
+            title
+            content
+            updatedAt(formatString: "YYYY/MM/DD")
+            createdAt(formatString: "YYYY/MM/DD")
+            category {
+                name
+                id
+            }
+            eyecatch {
+                url
+            }
+        }
+        site {
+            siteMetadata {
+                siteUrl
+            }
+        }
     }
-  }
 `
--- src/pages/category/{microcmsCategories.categoriesId}.js
+++ src/pages/category/{microcmsCategories.categoriesId}.js
@@ -6,62 +6,67 @@ import Seo from "../../components/seo"
 import moment from "moment"
 
 const CategoryPage = ({ data }) => (
-  <Layout currentPage={data.microcmsCategories.categoriesId}>
-    <Seo title="Home"></Seo>
-    <ul className="c-main__list">
-      {data.allMicrocmsBlogs.edges.map(({ node }) => (
-        <li key={node.blogsId} className="c-main__listItem">
-          <Link
-            to={`/blog/${node.blogsId}`}
-            className="c-main__listItemWrapper"
-          >
-            <div
-              className={`c-main__listItemCategory is-${node.category.name}`}
-            >
-              {node.category.name}
-            </div>
-            <div className="c-main__listItemImage">
-              {node.eyecatch !== null && (
-                <img srcSet={node.eyecatch.url} alt={node.title} />
-              )}
-            </div>
-            <div className="c-main__listItemTitle">
-              {node.title}
-              <span className="c-main__listItemDate">
-                {moment(node.createdAt).format("YYYY年MM月DD日")}
-              </span>
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </Layout>
+    <Layout currentPage={data.microcmsCategories.categoriesId}>
+        <Seo title="Home"></Seo>
+        <ul className="c-main__list">
+            {data.allMicrocmsBlogs.edges.map(({ node }) => (
+                <li key={node.blogsId} className="c-main__listItem">
+                    <Link
+                        to={`/blog/${node.blogsId}`}
+                        className="c-main__listItemWrapper"
+                    >
+                        <div
+                            className={`c-main__listItemCategory is-${node.category.name}`}
+                        >
+                            {node.category.name}
+                        </div>
+                        <div className="c-main__listItemImage">
+                            {node.eyecatch !== null && (
+                                <img
+                                    srcSet={node.eyecatch.url}
+                                    alt={node.title}
+                                />
+                            )}
+                        </div>
+                        <div className="c-main__listItemTitle">
+                            {node.title}
+                            <span className="c-main__listItemDate">
+                                {moment(node.createdAt).format(
+                                    "YYYY年MM月DD日"
+                                )}
+                            </span>
+                        </div>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </Layout>
 )
 
 export default CategoryPage
 
 export const query = graphql`
-  query ($categoriesId: String!) {
-    allMicrocmsBlogs(
-      filter: { category: { id: { eq: $categoriesId } } }
-      sort: { createdAt: DESC }
-    ) {
-      edges {
-        node {
-          title
-          category {
-            name
-          }
-          blogsId
-          createdAt
-          eyecatch {
-            url
-          }
+    query ($categoriesId: String!) {
+        allMicrocmsBlogs(
+            filter: { category: { id: { eq: $categoriesId } } }
+            sort: { createdAt: DESC }
+        ) {
+            edges {
+                node {
+                    title
+                    category {
+                        name
+                    }
+                    blogsId
+                    createdAt
+                    eyecatch {
+                        url
+                    }
+                }
+            }
         }
-      }
+        microcmsCategories(categoriesId: { eq: $categoriesId }) {
+            categoriesId
+        }
     }
-    microcmsCategories(categoriesId: { eq: $categoriesId }) {
-      categoriesId
-    }
-  }
 `
