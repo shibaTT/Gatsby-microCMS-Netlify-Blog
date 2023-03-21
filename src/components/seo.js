9c429be7f26d204dc13028475953d95999b23483
s1622074@s.do-johodai.ac.jp <s1622074@s.do-johodai.ac.jp>
2023-03-21 09:55:38 +0900

    update: ファビコン追加

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,42 +10,45 @@ import { useStaticQuery, graphql } from "gatsby"
 import { Helmet } from "react-helmet"
 
 function Seo({ description, title, children }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-      }
-    `
-  )
+    const { site } = useStaticQuery(
+        graphql`
+            query {
+                site {
+                    siteMetadata {
+                        title
+                        description
+                        author
+                    }
+                }
+            }
+        `
+    )
 
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
+    const metaDescription = description || site.siteMetadata.description
+    const defaultTitle = site.siteMetadata?.title
 
-  return (
-    <>
-      <Helmet>
-        <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
-        <meta name="description" content={metaDescription} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={metaDescription} />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary" />
-        <meta
-          name="twitter:creator"
-          content={site.siteMetadata?.author || ``}
-        />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={metaDescription} />
-      </Helmet>
-      {children}
-    </>
-  )
+    return (
+        <>
+            <Helmet>
+                <title>
+                    {defaultTitle ? `${title} | ${defaultTitle}` : title}
+                </title>
+                <meta name="description" content={metaDescription} />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={metaDescription} />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:card" content="summary" />
+                <meta
+                    name="twitter:creator"
+                    content={site.siteMetadata?.author || ``}
+                />
+                <meta name="twitter:title" content={title} />
+                <meta name="twitter:description" content={metaDescription} />
+                <link rel="icon" href="/icon/favicon.ico" />
+            </Helmet>
+            {children}
+        </>
+    )
 }
 
 export default Seo
