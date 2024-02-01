308a4c69f40c2c0090aaa8786a3f1fb16c4ee67a
ShibaTT <39818834+shibaTT@users.noreply.github.com>
2024-02-01 12:35:45 +0900

    update: 整形

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,39 +11,39 @@
 require("dotenv").config()
 
 module.exports = {
-  siteMetadata: {
-    title: "To The First",
-    description:
-      "初めてのJamstackのブログを作ってみたので、その過程や躓いたところとか、あとは普通に健忘録的なものとか、日常とか、なんかよくわかんないけど適当に置いておこうかなって感じですね",
-    author: "@tori",
-    siteUrl: "https://ecup.netlify.app",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-microcms",
-      options: {
-        apiKey: process.env.API_KEY,
-        serviceId: "ecup",
-        apis: [
-          {
-            endpoint: "blogs",
-          },
-          {
-            endpoint: "categories",
-          },
-        ],
-      },
+    siteMetadata: {
+        title: "To The First",
+        description:
+            "初めてのJamstackのブログを作ってみたので、その過程や躓いたところとか、あとは普通に健忘録的なものとか、日常とか、なんかよくわかんないけど適当に置いておこうかなって感じですね",
+        author: "@tori",
+        siteUrl: "https://ecup.netlify.app",
     },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-react-helmet",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/images`,
-        name: "images",
-      },
-    },
-  ],
+    plugins: [
+        {
+            resolve: "gatsby-source-microcms",
+            options: {
+                apiKey: process.env.API_KEY,
+                serviceId: "ecup",
+                apis: [
+                    {
+                        endpoint: "blogs",
+                    },
+                    {
+                        endpoint: "categories",
+                    },
+                ],
+            },
+        },
+        "gatsby-plugin-sass",
+        "gatsby-plugin-react-helmet",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                path: `${__dirname}/src/images`,
+                name: "images",
+            },
+        },
+    ],
 }
